b254b6d74e0ca9d350504fe63b0dcd3e854d02ed
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-07-13 12:19:27 +0800

    mongodb connection

--- chat-app-client/server.js
+++ chat-app-client/server.js
@@ -3,10 +3,14 @@ var bodyParser = require("body-parser");
 var app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const mongoose = require("mongoose");
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const dbUrl =
+  "mongodb://<username>:<password>@ds343718.mlab.com:43718/learning-nod";
 
 var messages = [
   { name: "Ankit", message: "Hi" },
@@ -22,6 +26,10 @@ app.post("/messages", (req, res) => {
   io.emit("message", req.body);
   messages.push(req.body);
   res.sendStatus(200);
+});
+
+mongoose.connect(dbUrl, (err) => {
+  console.log("Mongodb connection", err);
 });
 
 server.listen(3000, () => {
